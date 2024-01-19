07667aa7b69264241ae3bbbff640e235016d19c2
luosrevlis <tuanminhphamtdn@gmail.com>
2024-01-19 21:29:00 +0700

    reservation dao and repo

---
+++ DAOs/ReservationDAO.cs
@@ -0,0 +1,56 @@
+﻿using BusinessObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAOs
+{
+    public class ReservationDAO
+    {
+        private readonly FuMiniHotelManagementContext _db;
+
+        public ReservationDAO(FuMiniHotelManagementContext db)
+        {
+            _db = db;
+        }
+
+        public List<BookingReservation> GetAll() => _db.BookingReservations.Include(res => res.Customer).ToList();
+
+        public BookingReservation? FindById(int id)
+        {
+            var res = _db.BookingReservations.Find(id);
+            if (res == null)
+            {
+                return null;
+            }
+            res.Customer = _db.Customers.Find(res.CustomerId)!;
+            res.BookingDetails = _db.BookingDetails
+                .Where(details => details.BookingReservationId == res.BookingReservationId).ToList();
+            return res;
+        }
+
+        public List<BookingReservation> FindByPredicate(Func<BookingReservation, bool> predicate)
+            => _db.BookingReservations.Where(predicate).ToList();
+
+        public void Add(BookingReservation reservation)
+        {
+            _db.BookingReservations.Add(reservation);
+            _db.SaveChanges();
+        }
+
+        public void Update(BookingReservation reservation)
+        {
+            _db.BookingReservations.Update(reservation);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var res = FindById(id);
+            if (res == null)
+            {
+                return;
+            }
+            _db.BookingReservations.Remove(res);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Repositories/Impl/ReservationRepository.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects;
+using DAOs;
+
+namespace Repositories.Impl
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly ReservationDAO _dao;
+
+        public ReservationRepository(ReservationDAO dao)
+        {
+            _dao = dao;
+        }
+
+        public void AddReservation(BookingReservation reservation) => _dao.Add(reservation);
+
+        public void DeleteReservation(int id) => _dao.Delete(id);
+
+        public BookingReservation? FindReservationById(int id) => _dao.FindById(id);
+
+        public List<BookingReservation> FindReservations(Func<BookingReservation, bool> predicate)
+            => _dao.FindByPredicate(predicate);
+
+        public List<BookingReservation> GetAllReservation() => _dao.GetAll();
+
+        public void UpdateReservation(BookingReservation reservation) => _dao.Update(reservation);
+    }
+}
